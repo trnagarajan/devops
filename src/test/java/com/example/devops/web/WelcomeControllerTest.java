0cd489e5c2f43c94feffee4e859f97ec0f9a05a0
trnagarajan <trnagarajan@gmail.com>
2024-11-24 16:40:20 +0530

    updated with scm and dist repo

---
+++ src/test/java/com/example/devops/web/WelcomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.devops.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+//@WebAppConfiguration
+public class WelcomeControllerTest {
+
+	/*
+	 * private MockMvc mockMvc;
+	 * 
+	 * @Autowired private WebApplicationContext context;
+	 * 
+	 * @Before public void setUp() throws Exception { mockMvc =
+	 * MockMvcBuilders.webAppContextSetup(context).build(); }
+	 * 
+	 * @Test public void testWelcome() throws Exception {
+	 * mockMvc.perform(get("/")).andExpect(status().isOk()); }
+	 */
+	@Test
+	void contextLoads1() {
+	}
+
+}
